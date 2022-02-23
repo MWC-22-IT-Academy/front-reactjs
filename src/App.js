9418dcc283c4e00b373829145d82e592347e344c
Silvia <silvia.piovesan@gmail.com>
2022-02-23 15:58:53 +0100

    test recharts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { data } from "./data-perros-gatos";
+import "./App.css";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineChart
+        width={1300}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="perros adoptados" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="gatos adoptados" stroke="#82ca9d" />
+      </LineChart>
     </div>
   );
 }
---
+++ src/data-perros-gatos.js
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    name: "Enero",
+    "perros adoptados": 27,
+    "gatos adoptados": 21,
+  },
+  {
+    name: "Febrero",
+    "perros adoptados": 49,
+    "gatos adoptados": 46,
+  },
+  {
+    name: "Marzo",
+    "perros adoptados": 26,
+    "gatos adoptados": 19,
+  },
+  {
+    name: "Abril",
+    "perros adoptados": 23,
+    "gatos adoptados": 14,
+  },
+  {
+    name: "Mayo",
+    "perros adoptados": 25,
+    "gatos adoptados": 48,
+  },
+  {
+    name: "Junio",
+    "perros adoptados": 14,
+    "gatos adoptados": 47,
+  },
+  {
+    name: "Julio",
+    "perros adoptados": 18,
+    "gatos adoptados": 32,
+  },
+  {
+    name: "Agosto",
+    "perros adoptados": 14,
+    "gatos adoptados": 47,
+  },
+  {
+    name: "Septiembre",
+    "perros adoptados": 31,
+    "gatos adoptados": 58,
+  },
+  {
+    name: "Octubre",
+    "perros adoptados": 29,
+    "gatos adoptados": 28,
+  },
+  {
+    name: "Noviembre",
+    "perros adoptados": 33,
+    "gatos adoptados": 29,
+  },
+  {
+    name: "Diciembre",
+    "perros adoptados": 36,
+    "gatos adoptados": 27,
+  },
+];
